9ec12973648236d3e93d3caaf2ce30de34d293d1
Dan O'Donovan <dan.odonovan@gmail.com>
2025-09-17 18:44:01 +0100

    Fix .config mapping: use dot_config/private_op; remove private_dot_config

--- home/private_dot_config/op/run_onchange_after_set_permissions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Set correct permissions for 1Password CLI config directory
-# 1Password CLI requires 700 permissions for security
-chmod 700 ~/.config/op
-chmod 600 ~/.config/op/config
